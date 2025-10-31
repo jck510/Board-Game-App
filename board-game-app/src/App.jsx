506c6b508bd0bf1190798c2481175319ff25b316
Joshua Knudsen <knudsen.joshua772@gmail.com>
2025-10-30 17:55:26 -0700

    Wrote the initial description for the board game

---
+++ board-game-app/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// BOARD GAME APP
+// Objective: Create a functioning board game web application that is playable by anyone who accesses the website
+// Mission: This is meant to be a creative project to display my ability to create a nice looking and interactive website
+/*
+Game Description: You are a local delivery professional who is on the verge of getting replaced.
+                  Your boss has given you the opportunity to show why you still deserve to keep your job.
+                  You must travel across the board linearly to deliver 4 packages in 6 turns or less per package.
+                  Your boss is giving you 3 chances until you are fired.
+                  Who knows what you may encounter along your journey that may or may not help you along the way
+                  
+
+Game Details:
+1 player
+Travel: Linear traversal through the board using a 6 sided dice
+Board: A square shaped board that has 22 different spaces
+
+          
+
+
+*/
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Board Game</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
